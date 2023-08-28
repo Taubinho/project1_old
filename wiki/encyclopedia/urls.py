01cb82cf93d168f94ee2991b3be8317afdc7837c
Taubinho <golobmat@gmail.com>
2023-08-28 21:38:39 +0200

    first day

---
+++ wiki/encyclopedia/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("new_page", views.new_page, name="new_page"),
+    path("entry/<str:name>", views.entry, name="entry"),
+    path("random", views.random_entry, name="random"),
+    path("?q<str:term>", views.search, name="search")
+]
---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,35 @@
+import random
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, name):
+
+    content = util.get_entry(name)
+
+    if not content:
+        return render(request, "encyclopedia/error.html", {"error": name})
+
+    return render(request, "encyclopedia/entry.html", {"content": content, "title": name})
+
+
+def new_page(request):
+    return render(request, "encyclopedia/new_page.html")
+
+def random_entry(request):
+
+    ran_entry = random.choice(util.list_entries())
+
+    return render(request, "encyclopedia/entry.html", {"content": util.get_entry(ran_entry), "title": ran_entry})
+
+def search(request, term):
+    return render(request, "encyclopedia/search.html", {"search_term": term})
